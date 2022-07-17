70cd4a84e77515513e8b4b32b9ea3650cf87892b
smnodame <boonprakitchaikaew@gmail.com>
2022-07-17 13:45:58 +0700

    remove unused code

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 from aiohttp import web
 import requests
 import asyncio
-
-async def function(start_event, end_event):
-    # Waits until we get the start event command.
-    await start_event.wait()
-
-    # Run our function until we get the end_event being set to true.
-    index = 0
-    while not end_event.is_set():
-        print(f"Function running; index {index}.")
-        await asyncio.sleep(2)
-        index += 1
         
 async def health(request):
     return web.Response(text="<h1> Async Rest API using aiohttp : Health OK </h1>",
